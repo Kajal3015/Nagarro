5cf75bb60cd6b851fe24a28b6f79b53f03f03aa7
Kjala jha <jhaman9810@gmail.com>
2022-07-07 20:35:21 +0530

    poke commit

---
+++ Poke/script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    // if(document.getElementById('btn').textContent=="Do you want to see complete gallery"){
+        if (confirm("Press a button!")) {
+            window.location=('C:\Users\Kajal jha\Desktop\Nagarro\pokemon\index2.html')
+            }else{
+              gallery.style.display='none'
+          }
+    // }else{
+    //     if (confirm("Press a button!")) {
+    //         gallery.style.display='none'
+    //         document.getElementById('btn').innerHTML="Do you want to hide complete gallery"
+    //         }else{
+    //           gallery.style.display='block'
+    //       }
+    // }
+    
+  }
+
